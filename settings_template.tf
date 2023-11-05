6b609c63629be824dcd8d0c4b201bef98e192c0c
ushknn <uekiworks@gmail.com>
2023-11-06 01:39:56 +0900

    comment out

--- settings_template.tf
+++ settings_template.tf
@@ -1,13 +1,13 @@
-variable "project" {
-    type = string
-    default = "project"
-}
+# variable "project" {
+#     type = string
+#     default = "project"
+# }
 
-variable "region" {
-    type = string
-    default = "asia-northeast1"
-}
-variable "service_account" {
-    type = string
-    default = "user@developer.gserviceaccount.com"
-}
+# variable "region" {
+#     type = string
+#     default = "asia-northeast1"
+# }
+# variable "service_account" {
+#     type = string
+#     default = "user@developer.gserviceaccount.com"
+# }
